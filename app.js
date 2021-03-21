4adb15ee0fbb97e9f6bd8a2a7e5616fb2fa6cd6b
unknown <pawan.144prg@gmail.com>
2021-03-21 13:27:26 +0530

    my first commit to this project

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const path = require('path');
+engine = require('ejs-mate');
+const app=express();
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use( express.static( "public" ) );
+
+app.get("/",(req,res)=>{
+	res.render("templetes/home.ejs");
+});
+
+app.get("/aboutGCU",(req,res)=>{
+	res.render("templetes/about.ejs");
+})
+
+
+app.listen(5000,()=>{
+	console.log("Listining to port 5000");
+})
